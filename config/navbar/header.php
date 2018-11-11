0b64697d009ff331c58acc1dd2fb44f6f7abb667
Helena Isåfjäll <heis18@student.bth.se>
2018-11-11 15:50:38 +0100

    Lagt till testsida i navbar

--- config/navbar/header.php
+++ config/navbar/header.php
@@ -30,6 +30,11 @@ return [
                         "url" => "redovisning/kmom02",
                         "title" => "Redovisning för kmom02.",
                     ],
+                    [
+                        "text" => "Kmom03",
+                        "url" => "redovisning/kmom03",
+                        "title" => "Redovisning för kmom03.",
+                    ],
                 ],
             ],
         ],
@@ -47,6 +52,11 @@ return [
             "text" => "Verktyg",
             "url" => "verktyg",
             "title" => "Verktyg och möjligheter för utveckling.",
+        ],
+        [
+            "text" => "Test",
+            "url" => "test",
+            "title" => "Testsida.",
         ],
     ],
 ];
